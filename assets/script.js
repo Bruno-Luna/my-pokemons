4af0fd34a86b2898e760e31b66b2eedcded43205
isaiaszanoni <isaiasz.mus@gmail.com>
2021-09-29 07:50:28 -0300

    [Feature Isaias - Task: Funcao que altera a cor dos cards de acordo com o tipo do pokemon]

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,55 @@
 function loadpk() {
-    let pokeInput = document.querySelector('#pokeInput');
+    var pokeInput = document.querySelector('#pokeInput');
+
+    var nameOrId = pokeInput.value;
+    var url = 'http://pokeapi.co/api/v2/pokemon/' + nameOrId.toLowerCase();
+
+    fetch(url)
+        .then((response) => {           
+            return response.json();
+        })
+        .then((data) => {
+            document.getElementById('nome').innerHTML = data['name'].toLowerCase();
+            document.getElementById('numero').innerHTML ='#' + data['id'];
+            //let img = data['sprites']['other']['dream_world']['front_default'];
+            let img = data['sprites']['front_default'];
+            let imgStyle =  "width: 180px; height: 180px";
+            document.getElementById('pic').setAttribute('style', imgStyle);
+            document.getElementById('pic').setAttribute('src', img);
+        })
+        .catch((erro) => {
+            console.log("erro: " + erro);
+        })
+
+}
+
+function changeCardColor(){
+    var pokeInput = document.querySelector('#pokeInput');
 
     var nameOrId = pokeInput.value;
     var url = 'http://pokeapi.co/api/v2/pokemon/' + nameOrId.toLowerCase();
 
     fetch(url)
         .then((response) => {
-           
             return response.json();
         })
         .then((data) => {
-            console.clear;
-            console.log(data);
-            document.getElementById('nome').innerHTML = data['name'].toUpperCase();
-            document.getElementById('numero').innerHTML ='#' + data['id'];
-            let img = data['sprites']['front_default'];
-            let imgStyle =  "width: 180px; height: 180px";
-            document.getElementById('pic').setAttribute('style', imgStyle)
-            document.getElementById('pic').setAttribute('src', img);
+            let backgroundType = "card ";
+            let type = data['types']['0']['type']['name'];
+            backgroundType += type;
+            document.getElementById('card').setAttribute('class', backgroundType);
+            console.log(backgroundType)
+
         })
         .catch((erro) => {
             console.log("erro: " + erro);
         })
+};
+
+function todasFuncoes(){
+    loadpk();
+    changeCardColor();
 }
 
-document.getElementById('btn1').onclick = loadpk;
+document.getElementById('btn1').onclick = todasFuncoes
+
