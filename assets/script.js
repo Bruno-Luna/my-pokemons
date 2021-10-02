9a4946ec6b07e6204514b653309983aede9bfee1
Bruno Luna <luna.brsilva@gmail.com>
2021-10-01 21:09:25 -0300

    [Feature Bruno - Task: Implementação do botão limpar o card]

--- assets/script.js
+++ assets/script.js
@@ -5,15 +5,16 @@ function loadpk() {
     var url = 'http://pokeapi.co/api/v2/pokemon/' + nameOrId.toLowerCase();
 
     fetch(url)
-        .then((response) => {           
+        .then((response) => {
             return response.json();
         })
         .then((data) => {
             document.getElementById('nome').innerHTML = data['name'].toLowerCase();
-            document.getElementById('numero').innerHTML ='#' + data['id'];
+            document.getElementById('numero').innerHTML = '#' + data['id'];
+            document.getElementById('numero').setAttribute('class', 'estilo_numero')
             //let img = data['sprites']['other']['dream_world']['front_default'];
             let img = data['sprites']['front_default'];
-            let imgStyle =  "width: 180px; height: 180px";
+            let imgStyle = "width: 180px; height: 180px";
             document.getElementById('pic').setAttribute('style', imgStyle);
             document.getElementById('pic').setAttribute('src', img);
         })
@@ -23,7 +24,7 @@ function loadpk() {
 
 }
 
-function changeCardColor(){
+function changeCardColor() {
     var pokeInput = document.querySelector('#pokeInput');
 
     var nameOrId = pokeInput.value;
@@ -34,11 +35,11 @@ function changeCardColor(){
             return response.json();
         })
         .then((data) => {
-            let backgroundType = "card ";
-            let type = data['types']['0']['type']['name'];
-            backgroundType += type;
+            let backgroundType = "card "; 
+            let type = data['types']['0']['type']['name']; 
+            backgroundType += type; 
             document.getElementById('card').setAttribute('class', backgroundType);
-            console.log(backgroundType)
+          
 
         })
         .catch((erro) => {
@@ -46,10 +47,26 @@ function changeCardColor(){
         })
 };
 
-function todasFuncoes(){
+function todasFuncoes() {
     loadpk();
     changeCardColor();
 }
 
+function limpar() {
+    document.getElementById('card').setAttribute('class', 'card')
+
+    document.getElementById('nome').setAttribute('class', 'nome_default shadow m-auto-texto-card')
+    document.getElementById('nome').innerHTML = 'Escolha o seu'
+
+    let imgDefault = 'assets/img/project/pokeball.png'
+    document.getElementById('pic').setAttribute('src', imgDefault);
+    document.getElementById('pic').setAttribute('style', 'list-style: none;')
+
+    document.getElementById('numero').innerHTML = 'Pokémon'
+    document.getElementById('numero').setAttribute('class', 'numero_default shadow')
+
+}
+
 document.getElementById('btn1').onclick = todasFuncoes
+document.getElementById('btnLimpar').onclick = limpar
 
